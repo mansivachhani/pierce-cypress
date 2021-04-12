daa33d550407a8df45e03b2c0bbfa59176acd1c8
Mansi Vachhani <45923888+mansivachhani@users.noreply.github.com>
2021-04-12 08:18:19 +0200

    Add files via upload

---
+++ pagination.spec.js
@@ -0,0 +1,42 @@
+import Pagination from "../../page-objects/pagination";
+
+const DOMAINS = ['https://www.24mx.ie', 'https://www.24mx.pl'];
+
+DOMAINS.forEach((domain) => {
+
+    describe(`${domain}: Test Pagination feature`, () => {
+        const pagination = new Pagination();
+        beforeEach(() => {
+            cy.viewport('macbook-16')
+            cy.setCookie('GDPR', 'true');
+            cy.setCookie('_gat_UA-18469658-6', '1');
+            cy.visit(`${domain}/outlet`);
+        });
+
+        it('Verify the pagination component exists', function () {
+            pagination.getElement().should('be.visible');
+        });
+
+        it('Verify next page button click', function () {
+            cy.url().should('not.contain', '/outlet?page=2');
+            pagination.getNextButtonElement().should('be.visible').click();
+            cy.url().should('contain', '/outlet?page=2');
+        });
+
+        it('Verify previous page button click', function () {
+            cy.visit(`${domain}/outlet?page=2`);
+            cy.url().should('contain', '/outlet?page=2');
+            pagination.getPrevButtonElement().should('be.visible').click();
+            cy.url().should('contain', '/outlet');
+        });
+
+        it('Verify particular page number button click', function () {
+            cy.visit(`${domain}/outlet?page=2`);
+            cy.url().should('contain', '/outlet?page=2');
+
+            pagination.getPage3ButtonElement().should('be.visible').click();
+            cy.url().should('contain', '/outlet?page=3');
+        });
+    });
+
+});
